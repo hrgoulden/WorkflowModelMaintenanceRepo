cd414994dea07b915f8248b264aa31a45e0282bd
Luca Di Dio <didiocommaluca@gmail.com>
2017-11-08 18:27:44 -0600

    Update Task.js

--- Proof Of Concept/HTML items/js/Task.js
+++ Proof Of Concept/HTML items/js/Task.js
@@ -1,12 +1,59 @@
 
 class Task{
 
-
-	constructor()
+	/*
+		parameters
+			var name 					//string var of the task name, different from type
+			var taskType 				//distinction between the various types of tasks
+			var xPosition, yPosition 	//x and y offsets for task element
+	*/
+	constructor(name, taskType, xPosition, yPosition)
 	{
-		var WFTask;
-		var EntityTypeCode
+		var WFTask = name;
+		var EntityTypeCode = taskType;		
+		var xPos = xPosition;
+		var yPos = yPosition;
 	}
+	
+	setEntityTypeCode(EntityTypeCode)
+	{
+		this.EntityTypeCode = EntityTypeCode;
+	}
+	getEntityTypeCode()
+	{
+		return this.EntityTypeCode;
+	}
+
+	//name = string var
+	setTaskName(name)
+	{
+		this.WFTask = name;
+	}
+	getTaskName()
+	{
+		return this.WFTask;
+	}
+
+	/*
+		Parameters
+			x //the x offset within the parent element the task element is anchored to (should be the Workflow div)
+			y //y offset
+		call function on a stop method for the draggable()
+	*/
+	setTaskPosition(x, y)
+	{
+		this.xPos = x;
+		this.yPos = y;
+	}
+	getTaskPositionX()
+	{
+		return xPos;
+	}
+	getTaskPositionY()
+	{
+		return yPos;
+	}
+
 
 	/*constructor(name, )
 	{
@@ -28,30 +75,6 @@ class Task{
 		var DefaultPendGroup = 
 	}*/
 
-	setEntityTypeCode(EntityTypeCode)
-	{
-		this.EntityTypeCode = EntityTypeCode;
-	}
 
-	getEntityTypeCode()
-	{
-		return this.EntityTypeCode;
-	}
+}//end class Task
 
-	dbInsert()
-	{
-		//commit new link to db
-	}
-	dbSelect()
-	{
-		//populate link object with WFTaskLink db object
-	}
-	dbUpdate()
-	{
-		//update the fields of an existing link record
-	}
-	dbDelete()
-	{
-		//remove link record from db
-	}
-}
