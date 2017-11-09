d6652b3bfc4cb7b853b196215c3d16ec2ad218df
Luca Di Dio <didiocommaluca@gmail.com>
2017-11-08 18:27:21 -0600

    Update WFlow.js

--- Proof Of Concept/HTML items/js/WFlow.js
+++ Proof Of Concept/HTML items/js/WFlow.js
@@ -1,27 +1,73 @@
 
 class WFlow{
-	constructor(desc, )
+
+	/*
+		Parameters
+			desc 			//string var describing the workflow
+			
+	*/
+	constructor(desc )
 	{
 		var WFlowSeq = 	//primary key
 		var Description = desc;
 		var RecordStatus = 'V';
-		var EntityTypeCode = 
-		var CreateUserID =
+		//var EntityTypeCode = 
+		//var CreateUserID =
 		var CreateDate = getDate();
-		var ModUserID = 
+		//var ModUserID = 
 		var ModDate = getDate();
-		var InitialTaskSeq = 
+		//var InitialTaskSeq = 
+
+		var taskArr = [];
+		var linkArr = [];
 	}
 
-	get()
+	
+	//adds task/link obj. parameter to end of array
+	addTask(taskObj)
 	{
-
+		this.taskArr.push(taskObj);
 	}
-	set()
+	addLink(linkObj)
 	{
-		
+		this.linkArr.push(linkObj);
+	}
+	
+	getTaskArr()
+	{
+		return this.taskArr;
+	}
+	getLinkArr()
+	{
+		return this.linkArr;
 	}
 
+	//splice(starting index, number of elements to remove from the starting index)
+	removeTask(index)
+	{
+		if (index > -1) {
+    		taskArr.splice(index, 1);
+		}
+	}
+	removeLink(index)
+	{
+		if (index > -1) {
+    		linkArr.splice(index, 1);
+		}
+	}
+
+	getTask(index)
+	{
+		return taskArr[index];
+	}
+	getLink(index)
+	{
+		return linkArr[index];
+	}
+
+
+
+	/*
 	dbInsert()
 	{
 		//commit new link to db
@@ -38,5 +84,5 @@ class WFlow{
 	{
 		//remove link record from db
 	}
-	
-}
+	*/
+}//end class WFlow
